79f00602a9b7d0e241508367e3f40704136d7178
Jacob Malloy <sjakem@att.net>
2021-08-06 14:55:48 -0400

    add an install script for windows

---
+++ install.ps1
@@ -0,0 +1,8 @@
+Remove-Item  $env:APPDATA\wsltty -Force -Recurse -ErrorAction Ignore
+New-Item -Path $env:APPDATA\wsltty  -ItemType Junction -Value .\wsltty
+
+Remove-Item  $HOME\.ssh\config -ErrorAction Ignore
+New-Item -Path $HOME\.ssh\config  -ItemType HardLink -Value .\ssh\config
+
+Remove-Item  $HOME\.ssh\authorized_keys -ErrorAction Ignore
+New-Item -Path $HOME\.ssh\authorized_keys  -ItemType HardLink -Value .\ssh\authorized_keys

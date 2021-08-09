0b9907c16000177ceb4d8a023b024b60d3275a97
Jacob Malloy <sjakem@att.net>
2021-08-09 19:16:55 -0400

    work on cleaning up the performance system

--- config/mem-cpu.c
+++ config/mem-cpu.c
@@ -1,14 +1,74 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#define SLEEP_TIME 10000
 
 struct memory{
     uint64_t total,free;
 };
 
 struct cpu{
+    uint64_t idle,total;
+};
+
+struct data{
+    union{
+        uint64_t idle,free;
+    };
+    uint64_t total;
+};
+
+#if __linux__
+
+struct cpu_internal{
   uint64_t user,nice,system,idle,iowait,irq,softirq;
 };
+
+struct memory get_mem(){
+    struct memory returnValue;
+    FILE *fd = fopen("/proc/meminfo","r");
+    if(NULL==fd){
+        fprintf(stderr,"Failed to open /proc/meminfo\n");
+        exit(-1);
+    }
+    fscanf(fd,"MemTotal: %lu kB\nMemFree: %lu kB",&returnValue.total,&returnValue.free);
+    fclose(fd);
+    return returnValue;
+}
+
+struct cpu_internal get_cpu_internal(){
+    struct cpu_internal returnValue;
+    FILE *fd = fopen("/proc/stat","r");
+    if(NULL==fd){
+        fprintf(stderr,"Failed to open /proc/stat\n");
+        exit(-1);
+    }
+    fscanf(cpuFd,"cpu  %lu %lu %lu %lu %lu %lu %lu",&returnValue.user,&returnValue.nice,&returnValue.system,&returnValue.idle,&returnValue.idle,&returnValue.iowait,&returnValue.irq,&returnValue.softirq);
+    fclose(fd);
+    return returnValue;
+}
+
+struct cpu get_cpu(){
+    struct cpu_internal cpu_new,cpu_old,cpu_diff;
+    struct cpu returnValue;
+    cpu_old = get_cpu_internal();
+    usleep(SLEEP_TIME);
+    cpu_new = get_cpu_internal();
+    cpu_diff.user = cpu_new.user-cpu_old.user;
+    cpu_diff.nice = cpu_new.nice-cpu_old.nice;
+    cpu_diff.system = cpu_new.system-cpu_old.system;
+    cpu_diff.idle = cpu_new.idle-cpu_old.idle;
+    cpu_diff.iowait = cpu_new.iowait-cpu_old.iowait;
+    cpu_diff.irq = cpu_new.irq-cpu_old.irq;
+    cpu_diff.softirq = cpu_new.softirq-cpu_old.softirq;
+    returnValue.total = cpu_diff.user+cpu_diff.nice+cpu_diff.system+cpu_diff.idle+cpu_diff.iowait+cpu_diff.irq+cpu_diff.softirq;
+    returnValue.idle = cpu_diff.iowait+cpu_diff.idle;
+    return returnValue;
+}
+
+#endif
 
 int main(int argc, char* argv[]){
     struct memory currentMemory;
@@ -22,21 +82,7 @@ int main(int argc, char* argv[]){
         return -1;
     }
     fscanf(cpuFd,"cpu  %lu %lu %lu %lu %lu %lu %lu",&cpu_old.user,&cpu_old.nice,&cpu_old.system,&cpu_old.idle,&cpu_old.idle,&cpu_old.iowait,&cpu_old.irq,&cpu_old.softirq);
-    fscanf(fd,"MemTotal: %lu kB\nMemFree: %lu kB",&currentMemory.total,&currentMemory.free);
-    percentUsed = 1.0-(1.0*currentMemory.free/currentMemory.total);
-    usleep(10000);
-    fclose(cpuFd);
-    cpuFd = fopen("/proc/stat","r");
-    if(cpuFd==NULL){
-        fprintf(stderr,"Failed to open /proc/stat\n");
-        return -1;
-    }
-    fscanf(cpuFd,"cpu  %lu %lu %lu %lu %lu %lu %lu",&cpu_new.user,&cpu_new.nice,&cpu_new.system,&cpu_new.idle,&cpu_new.idle,&cpu_new.iowait,&cpu_new.irq,&cpu_new.softirq);
-    total = cpu_new.user-cpu_old.user+cpu_new.nice-cpu_old.nice+cpu_new.system-cpu_old.system+cpu_new.idle-cpu_old.idle+cpu_new.iowait-cpu_old.iowait+cpu_new.irq-cpu_old.irq+cpu_new.softirq-cpu_old.softirq;
     printf("Total:%lu - Free:%lu\nPercent used %lf\n",currentMemory.total,currentMemory.free,percentUsed);
-    percentUsed = 1.0-(1.0*(cpu_new.idle-cpu_old.idle)/total);
     printf("Total:%lu - Free:%lu\nPercent used %lf\n",total,cpu_new.idle,percentUsed);
-    fclose(fd);
-    fclose(cpuFd);
     return 0;
 }

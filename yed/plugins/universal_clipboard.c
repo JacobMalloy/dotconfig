06818ed65913e47d7bfbd755e10a2790fc79e5b2
Jacob Malloy <sjakem@att.net>
2021-11-10 16:18:28 -0600

    get universal clipboard working

--- yed/plugins/universal_clipboard.c
+++ yed/plugins/universal_clipboard.c
@@ -4,6 +4,12 @@
 #include<yed/plugin.h>
 
 void copy_to_universal_clipboard(int argc, char **argv);
+void cmd_pre_handler(yed_event *event);
+void var_post_change(yed_event *event);
+
+static int universal_copy_on_yank_state;
+static yed_event_handler copy_yank_pre;
+static yed_plugin *global_self;
 
 static const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 //code modified from stack overflow post to get the b4 bit encoded verison of a char array
@@ -106,8 +112,34 @@ char *get_sel_text(yed_buffer *buffer) {
 }
 
 int yed_plugin_boot(yed_plugin *self){
+    char * copy_on_yank;
+    yed_event_handler variable_change_event;
+
     YED_PLUG_VERSION_CHECK();
+
     yed_plugin_set_command(self, "copy-to-universal-clipboard", copy_to_universal_clipboard);
+
+    variable_change_event.kind = EVENT_VAR_POST_SET;
+    variable_change_event.fn = var_post_change;
+
+    copy_yank_pre.kind = EVENT_CMD_PRE_RUN;
+    copy_yank_pre.fn = cmd_pre_handler;
+
+    global_self = self;
+
+    copy_on_yank = yed_get_var("universal-copy-on-yank");
+
+    yed_plugin_add_event_handler(global_self, variable_change_event);
+
+    if(copy_on_yank == NULL){
+        yed_set_var("universal-copy-on-yank","NO");
+    }else if(yed_var_is_truthy("universal-copy-on-yank")){
+        yed_plugin_add_event_handler(global_self, copy_yank_pre);
+        universal_copy_on_yank_state=1;
+    }else{
+        universal_copy_on_yank_state=0;
+    }
+
     return 0;
 }
 
@@ -124,9 +156,29 @@ void copy_to_universal_clipboard(int argc, char **argv){
             out_buffer = malloc(strlen(base64)+20);
             snprintf(out_buffer,strlen(base64)+20,"\033]52;c;%s\a",base64);
             append_to_output_buff(out_buffer);
+            yed_cprint("Copied to system clipboard");
             free(out_buffer);
             free(selection);
             free(base64);
         }
+    }
+}
+
+void cmd_pre_handler(yed_event *event){
+    if(strcmp(event->cmd_name,"yank-selection")==0){
+        copy_to_universal_clipboard(0,NULL);
+    }
+}
+
+
+void var_post_change(yed_event *event){
+    int state = yed_var_is_truthy("universal-copy-on-yank");
+    if(state != universal_copy_on_yank_state){
+        if(state){
+            yed_plugin_add_event_handler(global_self,copy_yank_pre);
+        }else{
+            yed_delete_event_handler(copy_yank_pre);
+        }
+        universal_copy_on_yank_state=state;
     }
 }

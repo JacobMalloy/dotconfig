e19c22198c76277d0cb36ab2509eaa7f78fa9530
Jacob Malloy <sjakem@att.net>
2022-01-11 13:09:05 -0600

    fix an error where I thought I needed to escape }

--- yed/plugins/lang/syntax/rust.c
+++ yed/plugins/lang/syntax/rust.c
@@ -107,7 +107,7 @@ int yed_plugin_boot(yed_plugin *self) {
         APOP();
 
         APUSH("&code-fn-call");
-            REGEXSUB("([[:alpha:]_][[:alnum:]_]*!\{0,1\})[[:space:]]*\\(", 1);
+            REGEXSUB("([[:alpha:]_][[:alnum:]_]*!\{0,1})[[:space:]]*\\(", 1);
             REGEXSUB("([[:alpha:]_][[:alnum:]_]*!)[[:space:]]*\\[", 1);
         APOP();
 

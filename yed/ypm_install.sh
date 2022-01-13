12666ceed89656db09b81d2491c50d643ca631e1
Jacob Malloy <jmalloy1@vols.utk.edu>
2022-01-12 23:18:27 -0500

    no reason to leave old commented out code since I have git

--- yed/ypm_install.sh
+++ yed/ypm_install.sh
@@ -25,36 +25,8 @@ mkdir -p ${YED_CONFIG_DIRECTORY}/ypm/plugins/styles
 for word in $(<../ypm_list); do
     if [ -d "./ypm_plugins/${word}" ]; then
         echo "Cloning building and installing ${word}"
-        #git submodule update --remote ./ypm_plugins/${word} &
         bash -c "git submodule update --force ./ypm_plugins/${word} && cd ./ypm_plugins/${word} && ./build.sh && mv ./*.so ${YED_CONFIG_DIRECTORY}/ypm/plugins/${word}.so" &
     fi
 done
 
 wait
-
-#cat ../ypm_list | xargs printf -- "ypm_plugins/%s " | xargs git submodule update --init --remote # I really liked this way of doing it, but it was single threaded and slow
-
-
-# for word in $(<../ypm_list); do
-#     if [ -d "./ypm_plugins/${word}" ]; then
-#         echo "building ${word}"
-#         cd ./ypm_plugins/${word}
-#         ./build.sh &
-#         cd ${YED_CONFIG_DIRECTORY}/ypm
-#     fi
-# done
-#
-# wait
-
-#cd ${YED_CONFIG_DIRECTORY}/ypm/ypm_plugins
-
-# mkdir -p ${YED_CONFIG_DIRECTORY}/ypm/plugins/lang/syntax
-# mkdir -p ${YED_CONFIG_DIRECTORY}/ypm/plugins/lang/tools
-# mkdir -p ${YED_CONFIG_DIRECTORY}/ypm/plugins/styles
-
-#echo "copying binaries"
-# find . -maxdepth 2 -name "*.so" -exec mv {} ${YED_CONFIG_DIRECTORY}/ypm/plugins \;
-# find ./lang/ -maxdepth 2 -name "*.so" -exec mv {} ${YED_CONFIG_DIRECTORY}/ypm/plugins/lang \;
-# find ./lang/syntax/ -maxdepth 2 -name "*.so" -exec mv {} ${YED_CONFIG_DIRECTORY}/ypm/plugins/lang/syntax \;
-# find ./lang/tools/ -maxdepth 2 -name "*.so" -exec mv {} ${YED_CONFIG_DIRECTORY}/ypm/plugins/lang/tools \;
-# find ./styles/ -maxdepth 2 -name "*.so" -exec mv {} ${YED_CONFIG_DIRECTORY}/ypm/plugins/styles \;

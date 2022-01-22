9bda3bb201a7ac23b621f4008779b5c5d9b06f43
Jacob Malloy <sjakem@att.net>
2022-01-22 15:52:10 -0500

    add a list unsaved to utils

--- yed/plugins/utils.c
+++ yed/plugins/utils.c
@@ -2,11 +2,13 @@
 
 /*commands*/
 static void write_all(int argc, char **argv);
+static void log_unsaved(int argc, char **argv);
 /*end_commands*/
 
 //set the commands in this block so that command creation is uniform
 void create_commands(yed_plugin *self){
     yed_plugin_set_command(self,"write-all",write_all);
+    yed_plugin_set_command(self,"log-unsaved",log_unsaved);
 }
 
 int yed_plugin_boot(yed_plugin *self){
@@ -35,5 +37,21 @@ static void write_all(int argc, char **argv){
         }
     }
 
+}
+static void log_unsaved(int argc, char **argv){
+    yed_buffer *buffer;
+    tree_it( yed_buffer_name_t, yed_buffer_ptr_t ) buffer_iterator;
+LOG_CMD_ENTER("log-unsaved");
+    tree_traverse( ys->buffers, buffer_iterator )
+    {
+        buffer = tree_it_val( buffer_iterator );
+
+        if ( !( buffer->flags & BUFF_SPECIAL ) && (buffer->flags & BUFF_MODIFIED))
+        {
+            yed_cprint("%s\n",buffer->name);
+
+        }
+    }
+LOG_EXIT();
 }
 /*End Implementations*/

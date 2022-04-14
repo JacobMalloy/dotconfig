ca16cf5550149d9e8402748e086cc1e8a2de1515
Jacob Malloy <sjakem@att.net>
2022-04-13 20:55:17 -0400

    hard link the tmux config for the ut systems

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ ln -sf $DIR/gitignore $HOME/.gitignore
 echo "install tmux conf"
 
 $CC $CCFlags $DIR/config/mem-cpu.c -lm -o $HOME/.config/tmux-performance
-ln -sf $DIR/tmux.conf $HOME/.tmux.conf
+ln -f $DIR/tmux.conf $HOME/.tmux.conf
 
 echo "install zsh"
 mkdir -p $HOME/.config/zsh

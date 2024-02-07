c6a50f1c000a18ee1fe8f1f5c31e895ba9d134b3
Jacob Malloy <sjakem@att.net>
2024-02-07 09:13:06 -0500

    make some changes to the way my wezterm works

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -33,17 +33,18 @@ config.font_size = 16
 config.color_scheme = 'Dark+'
 config.default_cursor_style = "SteadyBar"
 
+
 config.colors={
     cursor_bg = '#ff2040',
     background = 'hsv(0,0%,2%)'
 }
 
-config.front_end = 'WebGpu'
+-- config.front_end = 'WebGpu'
 -- config.front_end = 'OpenGL'
 
 
-config.window_background_opacity = 0.8
-config.text_background_opacity=0.5
+--config.window_background_opacity = 0.8
+--config.text_background_opacity=0.5
 config.macos_window_background_blur = 20
 -- config.win32_system_backdrop = "Acrylic"
 
@@ -51,18 +52,73 @@ config.macos_window_background_blur = 20
 config.audible_bell = "Disabled"
 
 
+window_background_opacity = 0.8
+text_background_opacity=0.5
+
 
 if string.find(wezterm.target_triple,"windows") then
-  config.window_background_opacity = .5
-  config.text_background_opacity=config.window_background_opacity;
+  window_background_opacity = .5
+  text_background_opacity=config.window_background_opacity;
   config.win32_system_backdrop = "Acrylic"
   config.front_end="OpenGL"
 elseif string.find(wezterm.target_triple,"linux") then
-  config.window_background_opacity = 0.85
-  config.text_background_opacity=config.window_background_opacity;
+  window_background_opacity = 0.85
+  text_background_opacity=config.window_background_opacity;
 end
 
 
+wezterm.on('window-config-reloaded', function(window, pane)
+    -- approximately identify this gui window, by using the associated mux id
+    local id = tostring(window:window_id())
+
+    -- maintain a mapping of windows that we have previously seen before in this event handler
+    local seen = wezterm.GLOBAL.seen_windows or {}
+    -- set a flag if we haven't seen this window before
+    local is_new_window = not seen[id]
+    -- and update the mapping
+    seen[id] = true
+    wezterm.GLOBAL.seen_windows = seen
+
+    -- now act upon the flag
+    if is_new_window then
+        local overrides = window:get_config_overrides() or {}
+        if not overrides.text_background_opacity then
+            overrides.text_background_opacity = text_background_opacity
+            overrides.window_background_opacity = window_background_opacity
+        end
+        window:set_config_overrides(overrides)
+    end
+end)
+
+wezterm.on('toggle-text-bg-opacity', function(window, pane)
+    local overrides = window:get_config_overrides() or {}
+    if not overrides.text_background_opacity then
+        overrides.text_background_opacity = text_background_opacity
+        overrides.window_background_opacity = window_background_opacity
+    else
+        overrides.text_background_opacity = nil
+        overrides.window_background_opacity = nil
+    end
+    window:set_config_overrides(overrides)
+end)
+
+config.keys = {
+    {
+        key = '+',
+        mods = 'CMD',
+        action = wezterm.action.IncreaseFontSize,
+    },
+    {
+        key = '-',
+        mods = 'CMD',
+        action = wezterm.action.DecreaseFontSize,
+    },
+    {
+        key = 'B',
+        mods = 'CMD',
+        action = wezterm.action.EmitEvent 'toggle-text-bg-opacity',
+    },
+}
 
 -- and finally, return the configuration to wezterm
 return config

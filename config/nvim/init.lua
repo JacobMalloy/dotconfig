90b6eb241a0d635cbcf8f8c467e3e86ea860d5fd
Jacob Malloy <jmalloy1@vols.utk.edu>
2025-01-30 19:05:21 -0500

    work on nvim setup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,6 @@
 require("jmalloy.maps")
 require("jmalloy.lazy")
 require("jmalloy.settings")
+
+builder = require("jmalloy.personal.builder")
+builder.setup()
--- config/nvim/lua/jmalloy/settings.lua
+++ config/nvim/lua/jmalloy/settings.lua
@@ -8,6 +8,17 @@ local o = vim.opt
 o.number = true -- Print the line number in front of each line
 o.relativenumber = true -- Show the line number relative to the line with the cursor in front of each line.
 o.clipboard = "unnamedplus" -- uses the clipboard register for all operations except yank.
+global.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
 o.syntax = "on" -- When this option is set, the syntax with this name is loaded.
 o.autoindent = true -- Copy indent from current line when starting a new line.
 o.cursorline = true -- Highlight the screen line of the cursor with CursorLine.

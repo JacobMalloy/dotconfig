be4273d03975d34bbd99639ea3bbe1acb05d87cc
Jacob Malloy <sjakem@att.net>
2025-05-08 14:57:12 -0400

    no long auto check for updates

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -28,5 +28,9 @@ require("lazy").setup({
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "habamax" } },
+    checker = {
+        notify = false,
+        check_automatically = false,
+    },
     -- automatically check for plugin updates
 })

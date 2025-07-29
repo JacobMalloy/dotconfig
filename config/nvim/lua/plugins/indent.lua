396be9e16fa9dbf3ba9f1bbf7479be4b367eb86c
Jacob Malloy <jmalloy1@vols.utk.edu>
2025-07-29 17:58:43 -0400

    try to automatically find the indentation needed

---
+++ config/nvim/lua/plugins/indent.lua
@@ -0,0 +1,14 @@
+return {
+
+  {
+    "gpanders/editorconfig.nvim",
+    -- Example: You could add an event to load it faster
+    event = "BufReadPre",
+  },
+
+  {
+    "tpope/vim-sleuth",
+    event = "BufReadPre",
+  },
+
+}

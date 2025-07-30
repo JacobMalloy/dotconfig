e488d24ccca11a42151b9353c7a3536fbd0814bf
Jacob Malloy <sjakem@att.net>
2025-07-30 11:47:12 -0400

    turn off treesitter indent

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
-      indent = { enable = true },
+      indent = { enable = false },
       autotag = {
         enable = true,
       },

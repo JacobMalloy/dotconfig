8659a16121268b23c0eb9609f039a1a7006cc64b
Jacob Malloy <sjakem@att.net>
2025-11-19 13:35:49 -0500

    add harper_ls to my neovim

--- config/nvim/lua/lazy_plugins/lsp.lua
+++ config/nvim/lua/lazy_plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
 
-        local lsps = {"clangd","rust_analyzer","basedpyright","lua_ls","texlab"}
+        local lsps = {"clangd","rust_analyzer","basedpyright","lua_ls","texlab","harper_ls"}
         for _,lsp in ipairs(lsps)do
             vim.lsp.enable(lsp);
         end
@@ -16,7 +16,7 @@ return {
                 -- to define small helper and utility functions so you don't have to repeat yourself.
                 --
                 -- In this case, we create a function that lets us more easily define mappings specific
-                -- for LSP related items. It sets the mode, buffer and description for us each time.
+                -- for LSP related items. It sets the mode, buffer, and description for us each time.
                 local map = function(keys, func, desc, mode)
                     mode = mode or 'n'
                     vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
@@ -27,7 +27,7 @@ return {
                 --
                 -- When you move your cursor, the highlights will be cleared (the second autocommand).
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
-                if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+                if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
                     local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
                     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                         buffer = event.buf,
@@ -53,8 +53,8 @@ return {
                 -- The following code creates a keymap to toggle inlay hints in your
                 -- code, if the language server you are using supports them
                 --
-                -- This may be unwanted, since they displace some of your code
-                if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+                -- This maybe unwanted, since they displace some of your code
+                if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
                     map('<leader>th', function()
                         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
                     end, '[T]oggle Inlay [H]ints')
